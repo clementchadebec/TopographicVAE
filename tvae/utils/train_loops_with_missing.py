f9f8ae0f9856f30109756a64ef7a710d4a4b27ab
clementchadebec <clement.chadebec@orange.fr>
2023-01-04 23:04:28 +0100

    change to sum

--- tvae/utils/train_loops_with_missing.py
+++ tvae/utils/train_loops_with_missing.py
@@ -198,13 +198,13 @@ def eval_epoch(model, val_loader, log, savepath, epoch, n_is_samples=100,
 
             rec_loss_seq = (rec_loss_seq.reshape(inputs['data'].shape[0], -1) * (1 - seq_mask)).sum(dim=-1)
             
-            total_miss = (1 - pix_mask * seq_mask.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)).sum()
+            #total_miss = (1 - pix_mask * seq_mask.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)).sum()
 
-            print(total_miss)
+            #print(total_miss)
 
 #            avg_KLD = (kl_z.sum() + kl_u.sum()) / x_batched.shape[0]
             avg_neg_logpx_z = neg_logpx_z.sum() / x_batched.shape[0]
-            avg_missing_neg_lodpx_z = (rec_loss_seq + rec_loss_pix).sum() / total_miss.item()
+            avg_missing_neg_lodpx_z = (rec_loss_seq + rec_loss_pix).sum()# / total_miss.item()
 #            eq_loss = all_pairs_equivariance_loss(s, bsz=x.shape[0], seq_len=x.shape[1], 
 #                                                  n_caps=model.grouper.n_caps, cap_dim=model.grouper.cap_dim)
 #
